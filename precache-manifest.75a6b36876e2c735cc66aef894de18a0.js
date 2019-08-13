cfe6a9fa675a428c742c54bf702b3a01b902fd69
Greyti <47736158+Greyti8888@users.noreply.github.com>
2019-08-13 13:04:52 +0300

    Updates

---
+++ precache-manifest.75a6b36876e2c735cc66aef894de18a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68fb3fb46648194d2d4519e5ddcc62f",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "36e7994c1b9d834c00f8",
+    "url": "/react-calculator/static/js/2.7beb52a3.chunk.js"
+  },
+  {
+    "revision": "e6045c29eb81b9f04a0a",
+    "url": "/react-calculator/static/js/main.0bc72664.chunk.js"
+  },
+  {
+    "revision": "2c7460d17796b892bd56",
+    "url": "/react-calculator/static/js/runtime~main.a948531e.js"
+  }
+]);
